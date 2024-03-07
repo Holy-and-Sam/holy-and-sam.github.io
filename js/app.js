726eb3052f8445a828d8493e21b38bf62d8e9918
Holy Lovenia <holy.lovenia@gmail.com>
2024-03-07 19:19:22 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -709,7 +709,7 @@ const comment = (() => {
         <div class="card-body bg-light shadow p-3 m-0 rounded-4" data-parent="true" id="${data.uuid}">
             <div class="d-flex flex-wrap justify-content-between align-items-center">
                 <p class="text-dark text-truncate m-0 p-0" style="font-size: 0.95rem;">
-                    <strong class="me-1">${util.escapeHtml(data.nama)}</strong><i class="fa-solid ${data.hadir ? 'fa-circle-check text-success' : 'fa-circle-xmark text-danger'}"></i>
+                    <strong class="me-1">${util.escapeHtml(data.nama)}</strong><i class="fa-solid ${data.hadir ? 'fa-solid fa-people-roof text-success' : 'fa-solid fa-child-reaching text-danger'}"></i>
                 </p>
                 <small class="text-dark m-0 p-0" style="font-size: 0.75rem;">${data.created_at}</small>
             </div>
